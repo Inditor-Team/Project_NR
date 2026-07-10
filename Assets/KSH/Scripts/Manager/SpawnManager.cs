f64d6ae49690d6c186e44e6a84148175446170f6
Seheyon <104759146+Seheyon@users.noreply.github.com>
2026-07-10 21:40:15 +0900

    [Feat] Manager Script 추가

---
+++ Assets/KSH/Scripts/Manager/SpawnManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
