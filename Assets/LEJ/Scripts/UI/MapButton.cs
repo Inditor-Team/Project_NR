08100ac7d025a62fec7ee0a1807d93e6d85dfcbb
usernumber257 <usernumber257@gmail.com>
2026-08-27 00:45:55 +0900

    [Resource] 프로토콜 선택 UI 변경

--- Assets/LEJ/Scripts/UI/MapButton.cs
+++ Assets/LEJ/Scripts/UI/MapButton.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,10 +6,13 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class MapButton : MonoBehaviour
 {
+    public string stageName;
     [SerializeField] GameObject lockIcon;
     [SerializeField] GameObject mapIcon;
     public SceneController.Scene sceneName;
+    [SerializeField] GameObject alertPanel;
     [SerializeField] Button stageAlertConfirmButton;
+    [SerializeField] TMP_Text stageAlertConfirmText;
 
     private void Start()
     {
@@ -18,6 +22,8 @@ public class MapButton : MonoBehaviour
     }
     public void OnClick()
     {
+        UIManager.Instance.Show(alertPanel);
+        stageAlertConfirmText.text = $"{stageName} 로 이동하시겠습니까?";
         stageAlertConfirmButton.onClick.RemoveAllListeners();
         stageAlertConfirmButton.onClick.AddListener(() => { SceneController.Instance.ChangeScene(sceneName); });
     }
