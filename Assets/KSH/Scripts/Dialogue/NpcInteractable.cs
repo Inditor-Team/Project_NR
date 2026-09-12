0f90738cd8703d33a7e8145846d253925503c6ed
Seheyon <104759146+Seheyon@users.noreply.github.com>
2026-09-12 21:50:32 +0900

    [Resource] 인트로 대화 추가

--- Assets/KSH/Scripts/Dialogue/NpcInteractable.cs
+++ Assets/KSH/Scripts/Dialogue/NpcInteractable.cs
@@ -4,7 +4,8 @@ public enum DialogueID
 {
     store_npc_1,
     event_npc_A_1,
-    event_npc_B_1
+    event_npc_B_1,
+    intro_npc_1
 }
 
 public class NpcInteractable : MonoBehaviour, IInteractable
